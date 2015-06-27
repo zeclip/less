a1063b03233f5cd1e13d6d30c948e92a21d7eae3
elorenzo <zeclip.l@gmail.com>
2015-06-27 10:10:25 -0430

    Incicializamos el proyecto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet/less" type="text/css" href="estilos.less">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+</head>
+<body>
+	<div id="wrap">
+		<h1>Este es nuestro header!</h1>
+
+		<div id="columna">
+			<p>Aqui va nuestro texto ... Lorem Impsum dolor ....</p>
+		</div>
+		<h2>Este es nuestro header!</h2>
+		<div id="columnas">
+			<p>Aqui va nuestro texto ... Lorem Impsum dolor ....</p>
+		</div>
+		<div id="columnas">
+			<p>Aqui va nuestro texto ... Lorem Impsum dolor ....</p>
+		</div>
+	</div>
+</body>
+</html>
